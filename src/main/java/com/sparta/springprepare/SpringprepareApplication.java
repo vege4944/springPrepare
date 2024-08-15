fd3f50b881fe2df5d06867c129289f6b1b8e9c14
vege4944 <vege4944@gmail.com>
2024-08-15 18:36:59 +0900

    firstCommit

---
+++ src/main/java/com/sparta/springprepare/SpringprepareApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springprepare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprepareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprepareApplication.class, args);
+	}
+
+}
